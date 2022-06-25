99aa1b8155f5bbe3189b32ef72f3a472c97c96bf
Carlos Andres Rivera <cr4185320@gmail.com>
2022-06-25 13:00:34 -0500

    hide critical processes

--- code/cproc.c
+++ code/cproc.c
@@ -17,14 +17,26 @@ void main(int argc, char **argv) {
 
     Process32First(snapshot_handle, &process_entry);
 
-    printf("Name:               PID:\n");
-    printf("===========================\n");
+    printf("                  Name:                    PID:\n");
+    printf("======================================== =======\n");
     for (;;) {
-        // HANDLE process_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, process_entry.th32ProcessID);
+        HANDLE process_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, process_entry.th32ProcessID);
 
-        printf("%-32s %u\n", process_entry.szExeFile, process_entry.th32ProcessID);
+        if (process_handle == INVALID_HANDLE_VALUE) {
+            break;
+        }
+        
+        BOOL is_critical;
+        IsProcessCritical(process_handle, &is_critical);
+        if (is_critical) {
+            Process32Next(snapshot_handle, &process_entry);
+            CloseHandle(process_handle);
+            continue;
+        }
 
-        // CloseHandle(process_handle);
+        printf("%-40s %u\n", process_entry.szExeFile, process_entry.th32ProcessID);
+
+        CloseHandle(process_handle);
         if (Process32Next(snapshot_handle, &process_entry) == 0) {
             break;
         }
